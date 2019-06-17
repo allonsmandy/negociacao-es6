32096b1748f397e780102cff8d79bd7a19d53e14
Amanditas <allonsmandy@gmail.com>
2019-06-17 18:20:05 -0300

    Resolvendo um problema com o paradigma funcional

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -13,7 +13,9 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        let data = new Date(this._inputData.value.replace(/-/g, ','));
+        let data = new Date(...this._inputData.value.split('-').map(function(item, indice) {
+                return item - indice % 2;
+        }));
         console.log(data);
     }
 
