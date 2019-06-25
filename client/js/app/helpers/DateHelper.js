f69c267e074e9b9c3ad56ab4af5fe2037ca07e0c
Amanditas <allonsmandy@gmail.com>
2019-06-24 21:40:40 -0300

    Template String

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -7,11 +7,12 @@ class DateHelper {
     }
 
     static textoParaData(texto) {
+        if (!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error ("Deve estar no formato YYYY-mm-dd");
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 
     static dataParaTexto(data) {
-        data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear();
+        return `${data.getDate()}/${(data.getMonth() + 1)}/${data.getFullYear()}`;
     }
 
 }
