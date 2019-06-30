e38417cf16a8bc5e7064b9dbb9c34cde89427374
Amanditas <allonsmandy@gmail.com>
2019-06-29 22:11:41 -0300

    Herança e reutilização de código

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,7 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
     constructor(elemento) {
-        this._elemento = elemento;
+        super(elemento);
     }
 
     _template(model) {
@@ -42,10 +42,6 @@ class NegociacoesView {
         </tfoot>
         </table> 
         `;
-    }
-
-    update(model) {
-        this._elemento.innerHTML = this._template(model);
     }
 
 }
