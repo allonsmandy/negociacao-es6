dc9e3ed2e7c50c8ec2ed3e1eee9b8c8453dedf2e
Mandinha <allonsmandy@gmail.com>
2019-12-29 08:05:45 -0300

    arrow function e seu escopo léxico

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,19 +1,14 @@
 class ListaNegociacoes {
 
-    constructor(contexto, armadilha) {
+    constructor(armadilha) {
 
         this._negociacoes = []
         this._armadilha = armadilha //será uma função
-        this._contexto = contexto
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
-        // this._armadilha(this)
-
-        Reflect.apply(this._armadilha, this._contexto, [this])
-
-        console.log(this)
+        this._armadilha(this)
     }
 
     get negociacoes() {
@@ -22,9 +17,6 @@ class ListaNegociacoes {
 
     esvazia() {
         this._negociacoes = []
-
-        Reflect.apply(this._armadilha, this._contexto, [this])
-
-        // this._armadilha(this)
+        this._armadilha(this)
     }
 }
