8f8abb64d3aef0744b84fc960cfbc82799d4548d
Mandinha <allonsmandy@gmail.com>
2019-12-29 07:51:11 -0300

    API Reflection e as facetas de this

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,19 @@
 class ListaNegociacoes {
 
-    constructor(armadilha) {
+    constructor(contexto, armadilha) {
 
         this._negociacoes = []
         this._armadilha = armadilha //será uma função
+        this._contexto = contexto
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
-        this._armadilha(this)
+        // this._armadilha(this)
+
+        Reflect.apply(this._armadilha, this._contexto, [this])
+
+        console.log(this)
     }
 
     get negociacoes() {
@@ -17,6 +22,9 @@ class ListaNegociacoes {
 
     esvazia() {
         this._negociacoes = []
-        this._armadilha(this)
+
+        Reflect.apply(this._armadilha, this._contexto, [this])
+
+        // this._armadilha(this)
     }
 }
