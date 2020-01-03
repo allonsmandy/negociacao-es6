333c2731135ce573bd7ed54a28648c6eaa3c0b32
Mandinha <allonsmandy@gmail.com>
2020-01-03 04:19:54 -0300

    isolando a complexidad de associar o modelo com a view na classe Bind

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,31 +7,21 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
-        // this._listaNegociacoes = new ListaNegociacoes((model) => {
-        //     this._negociacoesView.update(model);
-        // });
-
-        // let self = this
-
-        this._listaNegociacoes = ProxyFactory.create(
-            new ListaNegociacoes(),
-            ['adiciona', 'esvazia'], (model) => {
-                this._negociacoesView.update(model)
-            }
-        )
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-
-        this._mensagem = ProxyFactory.create(
-            new Mensagem(),
-            ['texto'], (model) => {
-                this._mensagemView.update(model);
-            }
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            this._negociacoesView,
+            ['adiciona', 'esvazia']
         )
 
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem)
+        this._mensagem = new Bind(
+            new Mensagem(), //model
+            this._mensagemView, //view
+            ['texto'] //condiçao
+        )
+
     }
 
     adiciona(event) {
@@ -40,7 +30,6 @@ class NegociacaoController {
         this._listaNegociacoes.adiciona(this._criaNegociacao());
 
         this._mensagem.texto = "Negociação adicionada com sucesso";
-        // this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
 
         console.log(this._listaNegociacoes.negociacoes);
